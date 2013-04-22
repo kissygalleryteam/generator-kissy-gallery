6a9d321fd360efa570881fe8652d51ede48ef223
maxbbn <qipbbn@gmail.com>
2013-04-22 22:17:21 +0800

    update

--- app/index.js
+++ app/index.js
@@ -46,18 +46,96 @@ prt.welcome = function() {
     console.log(welcome);
 }
 
-prt.readme = function(){
-  this.copy('README.md','README.md');
+AppGenerator.prototype.askFor = function askFor() {
+  var cb = this.async();
+
+  // welcome message
+  var welcome = "\n" +
+    "\n __  _  ____ _____ _____ __ __".cyan +
+    "\n|  |/ ]|    / ___// ___/|  |  |".cyan +
+    "\n|  ' /  |  (   \\_(   \\_ |  |  |".white +
+    "\n|    \\  |  |\\__  |\\__  ||  ~  |".white +
+    "\n|     | |  |/  \\ |/  \\ ||___, |".green +
+    "\n|  .  | |  |\\    |\\    ||     |".green +
+    "\n|__|\\_||____|\\___| \\___||____/".green +
+    "\n                                                 " +
+    "\n  ____   ____  _      _        ___  ____   __ __ " +
+    "\n /    | /    || |    | |      /  _]|    \\ |  |  |" +
+    "\n|   __||  o  || |    | |     /  [_ |  D  )|  |  |" +
+    "\n|  |  ||     || |___ | |___ |    _]|    / |  ~  |" +
+    "\n|  |_ ||  _  ||     ||     ||   [_ |    \\ |___, |" +
+    "\n|     ||  |  ||     ||     ||     ||  .  \\|     |" +
+    "\n|___,_||__|__||_____||_____||_____||__|\\_||____/ " +
+    "\n                                                 ";
+  console.log(welcome);
+
+  var abcJSON = {};
+
+  try {
+    abcJSON = require(path.resolve(process.cwd(), 'abc.json'));
+  } catch (e) {}
+
+  if (!abcJSON.author) {
+    abcJSON.author = {
+      name: '',
+      email: ''
+    }
+  }
+
+  var prompts = [{
+    name: 'projectName',
+    message: 'Name of Project?',
+    default: abcJSON.name || path.basename(process.cwd())
+  },{
+    name: 'author',
+    message: 'Author Name:',
+    default: abcJSON.author.name,
+    warning: ''
+  },{
+    name: 'email',
+    message: 'Author Email:',
+    default: abcJSON.author.email,
+    warning: ''
+  },{
+    name: 'styleEngine',
+    message: 'Whitch style engin do you use [css-combo|less|sass]?',
+    default: abcJSON._kissy_pie.styleEngine
+  }];
+
+  this.prompt(prompts, function (err, props) {
+    if (err) {
+      return this.emit('error', err);
+    }
+
+    // manually deal with the response, get back and store the results.
+    // we change a bit this way of doing to automatically do this in the self.prompt() method.
+    this.projectName = props.projectName;
+    this.author = props.author;
+    this.email = props.email;
+    this.styleEngine = props.styleEngine;
+    this.enableLess = (/less/i).test(this.styleEngine);
+    this.enableSass = (/sass/i).test(this.styleEngine);
+    this.enableCSSCombo = (/css-combo/i).test(this.styleEngine);
+
+    cb();
+
+  }.bind(this));
+};
+
+prt.pkgJSON = function(){
+  this.template('_package.json','package.json');
 }
 
 prt.abcJSON = function(){
   this.template('abc.json','abc.json');
 }
 
+prt.readme = function(){
+  this.copy('README.md','README.md');
+}
+
 prt.gruntfile = function(){
   this.template('Gruntfile.js','Gruntfile.js');
 }
 
-prt.pkgJSON = function(){
-    this.template('_package.json','package.json');
-}
+
