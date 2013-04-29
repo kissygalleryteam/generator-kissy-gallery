9d5167fecdf9f6298d4276780aff97e5ad98d326
minghe <minghe36@126.com>
2013-04-29 16:35:02 +0800

    modify

--- app/index.js
+++ app/index.js
@@ -6,16 +6,14 @@ var yeoman = require('yeoman-generator');
 module.exports = Gallery;
 function Gallery(args, options, config) {
     yeoman.generators.Base.apply(this, arguments);
-
-    this.hookFor('kissy-gallery:version', {
-        args:[false]
-    });
-
+    this.version = args[0] || '1.0';
+    this.cwd = options.env.cwd;
+    console.log(this.cwd);
+    this.componentName = getComName(this);
     this.on('end',function(){
         console.log("组件目录和文件初始化完成！")
-        console.log("\n打包调用：")
-        console.log("cd 组件目录")
-        console.log('grunt #打包你的组件')
+        console.log("\n打包组件运行：")
+        console.log('grunt')
     })
 }
 
@@ -24,8 +22,6 @@ util.inherits(Gallery, yeoman.generators.Base);
 var prt = Gallery.prototype;
 
 prt.askFor = function(){
-    var cb = this.async();
-
     //打印欢迎消息
     var welcome = "\n" +
         "\n __  _  ____ _____ _____ __ __".cyan +
@@ -45,27 +41,71 @@ prt.askFor = function(){
         "\n|___,_||__|__||_____||_____||_____||__|\\_||____/ " +
         "\n                                                 ";
     console.log(welcome);
-
-
+}
+prt.askAuthor = function(){
+    var cb = this.async();
     var prompts = [{
-        name: 'componentName',
-        message: 'name of component:'
+        name: 'author',
+        message: 'author of component:',
+        default: 'kissy-team'
+    },{
+        name: 'email',
+        message: 'email of author:',
+        default: 'kissy-team@gmail.com'
     }];
 
     this.prompt(prompts, function (err, props) {
         if (err) {
             return this.emit('error', err);
         }
-        //组件名称
-        this.componentName = props.componentName;
-        this.mkdir(this.componentName);
-        this.env.options.componentName = this.componentName;
+        this.author = props.author;
+        this.email = props.email;
         cb();
     }.bind(this));
 }
-
 prt.copyFile = function(){
-    this.copy('Gruntfile.js',this.componentName + '/Gruntfile.js');
-    this.copy('_.gitignore',this.componentName + '/.gitignore');
+    this.copy('Gruntfile.js','Gruntfile.js');
+    this.copy('_.gitignore','.gitignore');
+    this.template('abc.json','abc.json');
+    this.template('_package.json','package.json');
+    this.template('README.md', 'README.md');
+
 }
+
+prt.mk = function(){
+    var version = this.version;
+    this.mkdir(version);
+    var fold = ['demo','doc','spec','build','plugin','guide','meta'];
+    for(var i=0;i<fold.length;i++){
+        this.mkdir(path.join(version, fold[i]));
+    }
+}
+
+prt.createVersion = function(){
+    var version = this.version;
+    this.comConfig = comConfig(this);
+    this.template('index.js', path.join(version, 'index.js'));
+    this.template('alias.js', path.join(version, 'meta','alias.js'));
+    this.template('modules.js', path.join(version, 'meta','modules.js'));
+    this.template('index.md', path.join(version, 'guide', 'index.md'));
+    this.template('index.html', path.join(version, 'demo', 'index.html'));
+}
+
+function getComName(that){
+    var root = that.destinationRoot();
+    return 'demo';
+}
+
+function comConfig(that){
+    var jsonFile = './abc.json';
+    var sAbcJson = that.readFileAsString(jsonFile);
+    var comConfig = JSON.parse(sAbcJson);
+    var comName = comConfig.name;
+    if(!comName) return false;
+    var first = comName.substring(0,1).toUpperCase();
+    var componentClass = first + comName.substring(1);
+    comConfig.componentClass = componentClass;
+    return comConfig;
+}
+
 
--- version/index.js
+++ version/index.js
@@ -7,63 +7,30 @@ module.exports = AppGenerator
 
 function AppGenerator(args, options, config) {
     yeoman.generators.NamedBase.apply(this, arguments);
+    this.version = args[0];
 }
 
 util.inherits(AppGenerator, yeoman.generators.NamedBase);
 
-AppGenerator.prototype.askFor = function () {
-    var cb = this.async();
-    var prompts = [
-        {
-            name: 'version',
-            message: 'please input init version number ',
-            default: '1.0',
-            warning: '开始gallery之旅吧'
-        }
-    ];
-    this.prompt(prompts
-        , function (err, props) {
-            if (err) {
-                this.emit('error', err);
-            }
-            this.version = props.version;
-
-            cb();
-        }.bind(this));
+AppGenerator.prototype.comConfig = function(){
+    var jsonFile = './abc.json';
+    var sAbcJson = this.readFileAsString(jsonFile);
+    this.comConfig = JSON.parse(sAbcJson);
 }
 
-AppGenerator.prototype.initVersionDir = function () {
-    var componentName =  this.componentName = this.env.options.componentName;
-    var version = this.version;
-    if(componentName){
-        version = componentName + '/' + version;
-        this.template('abc.json',componentName + '/abc.json');
-        this.version = this.version;
 
-        this.template('_package.json',componentName + '/package.json');
-        this.template('README.md',componentName + '/README.md');
-        var root = this.destinationRoot(componentName);
-        this.installDependencies();
-        this.destinationRoot('../');
-    }else{
-        this.writeJson('./abc.json',function(json){
-            json.version = version;
-            return json;
-        });
-        this.writeJson('./package.json',function(json){
-            json.version = version+'.0';
-            return json;
-        });
-    }
-
-    mk(version,this);
-
-    this.comConfig = comConfig(this);
-    this.template('index.js', path.join(version, 'index.js'));
-    this.template('alias.js', path.join(version, 'meta','alias.js'));
-    this.template('modules.js', path.join(version, 'meta','modules.js'));
-    this.template('index.md', path.join(version, 'guide', 'index.md'));
-    this.template('index.html', path.join(version, 'demo', 'index.html'));
+AppGenerator.prototype.copy = function(){
+    var curVer = this.comConfig.version;
+    if(this.version == curVer) return false;
+    this.directory(curVer,this.version);
+    this.writeJson('./abc.json',function(json){
+        json.version = this.version;
+        return json;
+    });
+    this.writeJson('./package.json',function(json){
+        json.version = this.version+'.0';
+        return json;
+    });
 
 }
 
@@ -73,28 +40,4 @@ AppGenerator.prototype.writeJson = function(file,fnMap){
     var oAbcJson = JSON.parse(sAbcJson);
     oAbcJson = fnMap.call(this,oAbcJson);
     this.write(file,JSON.stringify(oAbcJson));
-}
-
-function comConfig(that){
-    var jsonFile = './abc.json';
-    if(that.env.options.componentName){
-        jsonFile = that.env.options.componentName + '/abc.json';
-    }
-    var sAbcJson = that.readFileAsString(jsonFile);
-    var comConfig = JSON.parse(sAbcJson);
-    var comName = comConfig.name;
-    if(!comName) return false;
-    var first = comName.substring(0,1).toUpperCase();
-    var componentClass = first + comName.substring(1);
-    comConfig.componentClass = componentClass;
-    return comConfig;
-}
-
-function mk(version,that){
-    if(!version) return false;
-    that.mkdir(version);
-    var fold = ['demo','doc','spec','build','plugin','guide','meta'];
-    for(var i=0;i<fold.length;i++){
-        that.mkdir(path.join(version, fold[i]));
-    }
 }
